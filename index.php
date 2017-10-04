478036f3fd3ae80270e9d139646fb2a5e5924e88
DenissK20 <deniska.riga@gmail.com>
2017-10-04 17:34:12 +0300

    File_1

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = $app->add('Button');
+/*$button = $app->add('Button');
 $button->set('button');
 $button->icon = 'plus';
 $button->link('https://www.youtube.com/');
-$button->set(['primary'=> true]);
+$button->set(['primary'=> true]);*/
+$name = 2;
+for ($L = 1; $L <= 9; $L++) {
+  $button = $app->add('Button');
+  $button->set(' '.$name);
+  $name = $name +2;
+  if (($L %2) ==1)  {
+    $button->set(['primary'=> true]);
+
+  }
+}
