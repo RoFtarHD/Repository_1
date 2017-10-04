80d0a93b2fa52a0cc3d477fbd3852ebd7a3fd517
DenissK20 <deniska.riga@gmail.com>
2017-10-04 17:00:59 +0300

    FIle_3

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = $app->add('button');
+$button = $app->add('Button');
 $button->set('button');
 $button->icon = 'plus';
