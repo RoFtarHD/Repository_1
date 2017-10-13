b6722f91a4a333a4add186892f4227dd0728e243
DenissK20 <deniska.riga@gmail.com>
2017-10-13 16:34:17 +0300

    r1

--- File_1.php
+++ File_1.php
@@ -1,6 +1,18 @@
 <?php
-require 'vendor.autoload.php'
+require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Lol');
 $app->initLayout('Centered');
-$button = $app ->add('Button');
-$button -> set('Button1');
+
+$button1 = $app ->add('Button');
+$button1 -> set('Button1');
+$button1 -> addClass('mini red');
+
+$button2 = $app ->add('Button');
+$button2 -> set('Button2');
+$button2 -> addClass('massive blue');
+
+$button3 = $app ->add('Button');
+$button3 -> set('Button3');
+$button3 -> addClass('big yellow');
+
+$button1 ->link('r1.txt');
